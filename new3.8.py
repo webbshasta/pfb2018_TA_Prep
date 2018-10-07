b4d7b42a486d26a59f85a21bc9486ae3b8744a54
Shasta Webb <Shasta@users-MacBook-Air.local>
2018-10-07 12:00:45 -0600

    adding new 3.8

---
+++ new3.8.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env/ python3
+
+myString = ('sapiens, erectus, neanderthalensis')
+
+print(myString)
+
+myList = myString.split(',')
+
+print(myList, 'is a', type(myList))
+
+print("\nDone.\n")
