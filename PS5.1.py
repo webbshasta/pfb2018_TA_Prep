ee2fb87b8f7a169f1b5571c273717aa44c22d5c3
Shasta Webb <Shasta@users-MacBook-Air.local>
2018-10-11 16:24:52 -0600

    adding new 5.1-5.3

---
+++ PS5.1.py
@@ -0,0 +1,29 @@
+#! usr/bin/env/ python3
+
+#Owner: Shasta Webb
+#Filename:PS5.1.py
+#Description: Write a script in which you construct a dictionary of your favorite things. Print your only your favorite book. Print out your favorite book but use a variable in the key. Add your favorite 'organism' to the dictionary. Make organism the new value of fav_thing. 
+
+favs = {
+		'book' : 'Lonesome Dove',
+		'song' : 'Only Skin',
+		'tree' : 'BSIM',
+		'organism' : 'cat'
+		}
+
+print(favs['book'])
+
+favThing = 'book'
+
+print(favs[favThing])
+
+favThing = 'organism'
+
+print(favs[favThing])
+
+print('\nDone.\n')
+
+
+
+
+ 
